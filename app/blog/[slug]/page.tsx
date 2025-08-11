74cfe7d31538083ec072ad8bccf03ad9584229d4
rose0503 <tqviet.0503@gmail.com>
2025-08-11 18:51:45 +0700

    update file page of blog

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,17 +1,19 @@
 type Params = {
-  params: {
+  params: Promise<{
     slug: string;
-  };
+  }>;
 };
 
 export async function generateMetadata({ params }: Params) {
-  return { title: `Post: ${params.slug}` };
+  const { slug } = await params;
+  return { title: `Post: ${slug}` };
 }
 
-export default function Page({ params }: Params) {
+export default async function Page({ params }: Params) {
+  const { slug } = await params;
   return (
     <>
-      <h1>Slug: {params.slug}</h1>
+      <h1>Slug: {slug}</h1>
       <p>VietTQ</p>
     </>
   );
